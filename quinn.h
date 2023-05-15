7de17226ff39108623273f41613e92784eb7c6ff
slowy07 <slowy.arfy@gmail.com>
2023-05-15 07:22:47 +0700

    chore: adding header files
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ quinn.h
@@ -0,0 +1,15 @@
+#ifndef QUINN_H
+#define QUINN_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+#define QUINN_VERSION "1.0.0"
+
+#define QUINN_F_NO_DIAG (0x001LL)
+#define QUINN_F_NO_DUAL (0x002LL)
+#define QUINN_F_CIGAR (0x004LL)
+
+#endif
+
